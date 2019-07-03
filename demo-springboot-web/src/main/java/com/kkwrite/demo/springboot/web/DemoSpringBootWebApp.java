6285255042544582f6214bae31d1ec472c505593
wangke <49186456@qq.com>
2019-07-03 23:10:02 +0800

    增加 demo-springboot-web

---
+++ demo-springboot-web/src/main/java/com/kkwrite/demo/springboot/web/DemoSpringBootWebApp.java
@@ -0,0 +1,13 @@
+package com.kkwrite.demo.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootWebApp.class, args);
+	}
+	
+}
